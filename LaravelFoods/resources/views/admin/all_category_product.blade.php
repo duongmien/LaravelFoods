6162ad642f6d7e8e97c50b6b0cbc38d170e4c0cb
nguyenson257 <nguyenson25701@gmail.com>
2022-01-12 14:01:52 +0700

    done sort table

--- LaravelFoods/resources/views/admin/all_category_product.blade.php
+++ LaravelFoods/resources/views/admin/all_category_product.blade.php
@@ -15,17 +15,17 @@ echo '<script>alert("'.$message.'");</script> ';
             Category List
         </div>
         <div class="row w3-res-tb">
+            
             <div class="col-sm-3">
+                <h4>Search</h4>
                 <div class="input-group">
-                    <input type="text" class="input-sm form-control" placeholder="Search">
-                    <span class="input-group-btn">
-                        <button class="btn btn-sm btn-default" type="button">Go!</button>
-                    </span>
+                <input type="search" oninput="filter_table(this, 'table_filter')" class="form_control"
+         placeholder="Filter This Table...">
                 </div>
             </div>
         </div>
         <div class="table-responsive">
-            <table class="table table-striped b-t b-light">
+            <table class="table _table table_sort  table-striped b-t b-light">
                 <thead>
                     <tr>
                         
@@ -35,7 +35,7 @@ echo '<script>alert("'.$message.'");</script> ';
                         <th style="width:30px;"></th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody id="table_filter">
                     <?php $i=1; ?> 
                     @foreach($all_category_product as $key => $cate_pro)
                     <tr>
@@ -69,24 +69,6 @@ echo '<script>alert("'.$message.'");</script> ';
                 </tbody>
             </table>
         </div>
-        <footer class="panel-footer">
-            <div class="row">
-
-                <div class="col-sm-5 text-center">
-                    <small class="text-muted inline m-t-sm m-b-sm">showing 20-30 of 50 items</small>
-                </div>
-                <div class="col-sm-7 text-right text-center-xs">
-                    <ul class="pagination pagination-sm m-t-none m-b-none">
-                        <li><a href=""><i class="fa fa-chevron-left"></i></a></li>
-                        <li><a href="">1</a></li>
-                        <li><a href="">2</a></li>
-                        <li><a href="">3</a></li>
-                        <li><a href="">4</a></li>
-                        <li><a href=""><i class="fa fa-chevron-right"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </footer>
     </div>
 </div>
 @endsection
