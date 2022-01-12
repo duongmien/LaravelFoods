05f182f94e7edb4797ccd33ef57656bcfb94c7e3
nguyenson257 <nguyenson25701@gmail.com>
2022-01-12 10:58:55 +0700

    upd statistical

--- LaravelFoods/app/Http/Controllers/AdminController.php
+++ LaravelFoods/app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
 session_start();
@@ -27,11 +28,11 @@ class AdminController extends Controller
     public function show_dashboard()
     {
         $this->AuthLogin();
-        return view('admin.dashboard');
-    }
-    public function logout()
-    {
-        $this->AuthLogin();
-        return view('admin.dashboard');
+        $c_p = DB::table('tbl_product')->where('product_status','1')->count();
+        $c_s = DB::table('tbl_product')->sum('product_sold');
+        $c_u = DB::table('tbl_user')->count();
+        $c_o = DB::table('tbl_order')->count();
+
+        return view('admin.dashboard')->with('c_p',$c_p)->with('c_s',$c_s)->with('c_u',$c_u)->with('c_o',$c_o);
     }
 }
--- LaravelFoods/resources/views/admin/dashboard.blade.php
+++ LaravelFoods/resources/views/admin/dashboard.blade.php
@@ -2,14 +2,14 @@
 @section('admin_content')
 <div class="market-updates">
                     <div class="col-md-3 market-update-gd">
-                        <a href="###">
+                        <a href="{{URL::to('/all-product')}}">
                         <div class="market-update-block clr-block-2">
                             <div class="col-md-4 market-update-right">
                                 <i class="fa fa-dropbox"> </i>
                             </div>
                             <div class="col-md-8 market-update-left">
                                 <h4>Product</h4>
-                                <h3>13,500</h3>
+                                <h3>{{($c_p)}}</h3>
                                 <p>Other hand, we denounce</p>
                             </div>
                             <div class="clearfix"> </div>
@@ -17,14 +17,14 @@
                         </a>
                     </div>
                     <div class="col-md-3 market-update-gd">
-                        <a href="###">
+                        <a href="{{URL::to('/all-product')}}">
                         <div class="market-update-block clr-block-1">
                             <div class="col-md-4 market-update-right">
                                 <i class="fa fa-usd"></i>
                             </div>
                             <div class="col-md-8 market-update-left">
                                 <h4>Sold</h4>
-                                <h3>1,250</h3>
+                                <h3>{{($c_s)}}</h3>
                                 <p>Other hand, we denounce</p>
                             </div>
                             <div class="clearfix"> </div>
@@ -32,14 +32,14 @@
                         </a>
                     </div>
                     <div class="col-md-3 market-update-gd">
-                        <a href="###">
+                        <a href="{{URL::to('/all-user')}}">
                         <div class="market-update-block clr-block-3">
                             <div class="col-md-4 market-update-right">
                                 <i class="fa fa-users"></i>
                             </div>
                             <div class="col-md-8 market-update-left">
                                 <h4>User</h4>
-                                <h3>1,500</h3>
+                                <h3>{{($c_u)}}</h3>
                                 <p>Other hand, we denounce</p>
                             </div>
                             <div class="clearfix"> </div>
@@ -47,14 +47,14 @@
                         </a>
                     </div>
                     <div class="col-md-3 market-update-gd">
-                        <a href="###">
+                        <a href="{{URL::to('/all-order')}}">
                         <div class="market-update-block clr-block-4">
                             <div class="col-md-4 market-update-right">
                                 <i class="fa fa-shopping-cart" aria-hidden="true"></i>
                             </div>
                             <div class="col-md-8 market-update-left">
                                 <h4>Orders</h4>
-                                <h3>1,500</h3>
+                                <h3>{{($c_o)}}</h3>
                                 <p>Other hand, we denounce</p>
                             </div>
                             <div class="clearfix"> </div>
